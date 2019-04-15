196ed275e12a063ba3082fe417f26af40d87aa25
Vadym  Kukhtin <vadkuhtin@gmail.com>
2019-04-15 21:19:43 +0200

    Add steemconnect login/logout, getProjects

--- src/routes/Login/Login.js
+++ src/routes/Login/Login.js
@@ -1,11 +1,23 @@
 import React from 'react';
-import { loginURL, isLoggedIn, username, logOut } from '../../services/SteemApi';
+import {
+  loginURL,
+  logOut,
+  steem_user,
+  steem_ac,
+  isLoggedIn,
+  getProjects,
+  getProjectDetails,
+  getLoggedUserInfo
+} from '../../services/SteemApi';
 
-export function Login() {
-    return (
-        <div>
-            {!isLoggedIn && <a href={loginURL}>Login</a>}
-            {isLoggedIn && <p>{username}: <a onClick={logOut}>Logout</a></p>}
-        </div>
-    );
+export function Login () {
+//   getProjects().then(res => console.log(res));
+//   getProjectDetails('spread-goood').then(res => console.log(res));
+getLoggedUserInfo();
+  return (
+    <div>
+      {!isLoggedIn && <a href={loginURL}>Login</a>}
+      {isLoggedIn && <p>{steem_user}: <a onClick={logOut}>Logout</a></p>}
+    </div>
+  );
 }
