7cdb7d37e92fa5fed55d121b927d69ccf7e9c4b0
AlexK <alexisinwork@gmail.com>
2019-04-29 21:24:30 +0200

    asd

--- src/routes/Home/Home.js
+++ src/routes/Home/Home.js
@@ -111,13 +111,13 @@ class Home extends React.Component {
                                     </h3>
 
                                     <p>
-                                        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                                        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-                                        when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-                                        It has survived not only five centuries, but also the leap into electronic typesetting,
-                                        remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset
-                                        sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like
-                                        Aldus PageMaker including versions of Lorem Ipsum.
+                                        Our vision is it to ease the individual achievement of social impact for planet and people.
+                                        We want to support the socially active with an infrastructure that lowers hurdles of the
+                                        project creation process and lets them focus solely on the challenge they chose to solve.
+                                        <br />
+                                        <br />
+                                        To achieve this, we help them to raise funds for their social projects and connect with the right
+                                        people within a purpose-driven community. All on one platform.
                                     </p>
                                 </Grid>
                             </Grid>
@@ -128,23 +128,28 @@ class Home extends React.Component {
                         <div className={`${classes.block} ${classes.blockColor1}`}>
                             <Grid container direction="row" alignItems="center" justify="center">
                                 <Grid item xs={6} className={classes.blockTextP}>
-                                    <h3 className={classes.title}>
-                                        How Does It Work
-                                    </h3>
+                                    <h3 className={classes.title}>How Does It Work</h3>
 
-                                    <p>
-                                        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                                        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-                                        when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-                                        It has survived not only five centuries, but also the leap into electronic typesetting,
-                                        remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset
-                                        sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like
-                                        Aldus PageMaker including versions of Lorem Ipsum.
-                                    </p>
+                                    <h4>Getting in touch</h4>
+                                    <p>Existing projects and people with ideas can post their state of development and demand for talent or funds. 
+                                    Users scan the projects’ database and get in touch directly with projects to become co-creators. 
+                                    In future matchmaking processes will support this getting together.</p>
+
+                                    <h4>Sharing talent</h4>
+                                    <p>Development of global project teams is supported through the platform. 
+                                    These teams can draw on best-practice, which is identified through community evaluation. Tools, e.g. 
+                                    for effective networking, project management and communication will be provided in future.</p>
+
+                                    <h4>Direct funding</h4>
+                                    <p>Direct project funding is enabled by transfer via blockchain. This makes funding a fast, 
+                                    cheap and personal experience. Using public blockchain ensures transparency of flow of 
+                                    funds and provides protection against fraudulos behaviour. this supports building trust between distant partners.</p>
+                                    <br/>
+                                    <p>Good projects will receive an extra-funding in form of rewards with project assessment based on community evaluation.</p>
                                 </Grid>
 
                                 <Grid item xs={6} className={classes.blockTextImg}>
-                                    <img style={{ width: '50%' }} src={HowImg} alt="How" />
+                                    <img style={{ width: '60%' }} src={HowImg} alt="How" />
                                 </Grid>
                             </Grid>
                         </div>
@@ -156,7 +161,8 @@ class Home extends React.Component {
                                 <img className={classes.homeCircle} src={PortraitImg} alt="Portrait" />
                                 <h3>Create project</h3>
                                 <p className={classes.homeCircleText}>
-                                    Some explanation
+                                    You have a project idea and look for talents or funds? Please <Link to={`/create-project`}>register</Link> a project account. Describe your project story by 
+                                    answering some questions, that help you tell your project story. Publish it and ask the community to engage in your project.
                                 </p>
                             </Grid>
 
@@ -164,24 +170,27 @@ class Home extends React.Component {
                                 <img className={classes.homeCircle} src={PortraitImg} alt="Portrait" />
                                 <h3>Find co-creators</h3>
                                 <p className={classes.homeCircleText}>
-                                    Some explanation
-                                    </p>
+                                    You’d like to invite people to co-create your project or idea? What fields of knowledge are you looking for? 
+                                    Do you need volunteers for specific tasks? Write an <Link to={`/create-project`}>update</Link> on your project story and describe your needs. 
+                                </p>
                             </Grid>
 
                             <Grid container item xs={12} sm={12} md={3} justify="center" alignItems="center" direction="column">
                                 <img className={classes.homeCircle} src={PortraitImg} alt="Portrait" />
                                 <h3>Reward social impact</h3>
                                 <p className={classes.homeCircleText}>
-                                    Some explanation
-                                    </p>
+                                    You want impactful projects to be visible? You want them to receive a little extra funding? Look through the project database and “like” 
+                                    those  you think are really good. They’ll be ranked higher and receive more visibility and some extra funding.
+                                </p>
                             </Grid>
 
                             <Grid container item xs={12} sm={12} md={3} justify="center" alignItems="center" direction="column">
                                 <img className={classes.homeCircle} src={PortraitImg} alt="Portrait" />
                                 <h3>Fund/Invest projects</h3>
                                 <p className={classes.homeCircleText}>
-                                    Some explanation
-                                    </p>
+                                    You are looking for a cheap and fast means of globally funding projects? Find your favorite project in our database. 
+                                    Register or login to your blockchain-account. Transfer blockchain currency within seconds.
+                                </p>
                             </Grid>
                         </Grid>
                     </div>
